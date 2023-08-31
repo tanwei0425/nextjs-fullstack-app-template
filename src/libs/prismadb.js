9c406a725cdfb09abe628a51a5398a45ac366526
tanwei@jilinxiangyun.com <tanwei@jilinxiangyun.com>
2023-08-31 15:10:23 +0800

    style: 完善备注

--- src/libs/prismadb.js
+++ src/libs/prismadb.js
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+// 防止，在开发模式下， PrismaClient 耗尽数据链接数，将实例化的 PrismaClient 对象存到全局 global
 const prismadb = globalThis.prisma || new PrismaClient()
 if (process.env.NODE_ENV !== "production") {
     globalThis.prisma = prismadb
